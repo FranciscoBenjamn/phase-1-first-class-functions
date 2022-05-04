96b67e0a2bb62d790c598511ab0d3f978e5211e2
Franklin Benjamin <franciscovibes14@gmail.com>
2022-05-04 03:05:09 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(spy){
+    return spy();
+}
+function returnsANamedFunction(){
+    return function name(){        
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    }
+}
